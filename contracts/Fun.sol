b290864e608e6cb6b6fc9af2733437e3f5bb8012
Tawnee <tawnee.a@proton.me>
2024-04-30 08:19:04 -0600

    listen to events

--- contracts/Fun.sol
+++ contracts/Fun.sol
@@ -4,11 +4,15 @@ pragma solidity 0.8.25;
 contract Fun {
   uint public x;
 
+  event XWasChanged(uint _from, uint _to);
+
   constructor(uint _x) {
+    emit XWasChanged(x, _x);
     x = _x;
   }
 
   function changeX(uint _x) external {
+    emit XWasChanged(x, _x);
     x = _x;
   }
 }
--- src/3-events.ts
+++ src/3-events.ts
@@ -1,23 +1,23 @@
 import { Hex, createWalletClient, getContract, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { arbitrumSepolia } from "viem/chains";
-import funJson from "../artifacts/Fun.json";
+import { sepolia } from "viem/chains";
+import funJson from "../artifacts/Fun";
 
 import dotenv from "dotenv";
 
-const { abi, bin } = funJson["contracts"]["contracts/Fun.sol:Fun"];
+const { abi } = funJson["contracts"]["contracts/Fun.sol:Fun"];
 
 dotenv.config();
 
 const privateKey = process.env.PRIVATE_KEY;
 const account = privateKeyToAccount(privateKey as Hex);
-const contractAddress = "0xf0de3c5d6fcf93f6fa4f5d201c9b42af72ed9088";
+const contractAddress = "0x1faeb776befc1b7cffe051948469d899eee40746";
 
 (async () => {
   const client = await createWalletClient({
     account,
     transport: http(process.env.API_URL),
-    chain: arbitrumSepolia,
+    chain: sepolia,
   });
 
   const contract = await getContract({
@@ -32,7 +32,7 @@ const contractAddress = "0xf0de3c5d6fcf93f6fa4f5d201c9b42af72ed9088";
 
   let x = 55n;
   setInterval(async () => {
-    await contract.write.changeX([x]);
     x++;
+    await contract.write.changeX([x]);
   }, 3000);
 })();
